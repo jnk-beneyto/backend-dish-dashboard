c8179ccd4ccaa1c08cd0f5244e91c96320f1ae38
J.C. Beneyto <jnk.beneyto@gmail.com>
2021-12-09 12:58:37 +0100

    adding mongoose

--- server.js
+++ server.js
@@ -1,22 +1,55 @@
-const helmet = require('helmet');
-const cors = require('cors');
-const express = require("express");
-const app = express();
+const helmet = require('helmet')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const express = require("express")
+const app = express()
+const {
+    DB
+} = require("./config")
+const {
+    DishRoutes
+} = require("./routes")
+
 
 //middlewares
 
-app.use(express.json());
-app.use(helmet());
-app.use(cors());
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
+
+mongoose.Promise = global.Promise
 
 //Welcoming route
 
 app.get('/', function (req, res) {
-  res.send({
-      status: 200,
-      message: "Welcome aboard!"
-  });
-  res.end();
-});
+    res.send({
+        status: 200,
+        message: "Welcome aboard!"
+    })
+    res.end()
+})
 
-module.exports = app;
+//routing
+
+app.use('/dish', DishRoutes)
+
+//uknown url 
+
+app.get('*', function (req, res) {
+    res.send({
+        status: 404,
+        message: "Not found!"
+    })
+    res.end()
+})
+
+//db conection
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).catch(
+    err => console.log("error " + err.message)
+)
+
+module.exports = app
